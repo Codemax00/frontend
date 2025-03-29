db15b9e01d88af9a543f57f15b3ceb67849cd166
Codemax00 <pathapatimohhith07@gmail.com>
2025-03-29 16:54:25 +0530

    removig eventlistener

--- events with js/eventswithjs.js
+++ events with js/eventswithjs.js
@@ -8,6 +8,17 @@ let bt=document.querySelector("#btn")
 bt.addEventListener("click",()=>{
     console.log("batten one is clicked")
 })
+bt.addEventListener("click",()=>{
+    console.log("clicked")
+})
+const thredEvent=()=>{
+    console.log("batten")
+}
+bt.addEventListener("click",thredEvent)
+bt.addEventListener("click",()=>{
+    console.log("click")
+})
+bt.removeEventListener("click",thredEvent)
 let div=document.querySelector("div");
 
 div.onmouseover= (e) => {
